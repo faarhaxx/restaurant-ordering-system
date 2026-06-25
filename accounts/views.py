f9ad683d9bcb461a61025c97055de96c2248dfb5
farhan <amanfarhan1234510@gmail.com>
2026-06-25 15:45:21 +0530

    Fix media images on render

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
@@ -11,14 +10,17 @@ def register_view(request):
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            user = form.save()
-            login(request, user)
+            form.save()
             return redirect('login')
+        else:
+            print(form.errors)
 
     else:
         form = RegisterForm()
 
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {
+        'form': form
+    })
 
 
 def login_view(request):
@@ -29,11 +31,15 @@ def login_view(request):
         if form.is_valid():
             login(request, form.get_user())
             return redirect('menu')
+        else:
+            print(form.errors)
 
     else:
         form = AuthenticationForm()
 
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'login.html', {
+        'form': form
+    })
 
 
 def logout_view(request):
